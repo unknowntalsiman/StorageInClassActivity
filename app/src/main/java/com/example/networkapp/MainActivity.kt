f9ee2ad54e402f13d59cf8bb1ff395f0274fd952
unknowntalsiman <tuo98558@temple.edu>
2025-04-08 12:06:52 -0400

    Implement comic caching so that it persists when user performs a rotation or restarting the activity.

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -6,22 +6,15 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
-import com.android.volley.Request
-import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
-
-// TODO (1: Fix any bugs)
-// TODO (2: Add function saveComic(...) to save comic info when downloaded
-// TODO (3: Automatically load previously saved comic when app starts)
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var requestQueue: RequestQueue
+    private lateinit var requestQueue: com.android.volley.RequestQueue
     lateinit var titleTextView: TextView
     lateinit var descriptionTextView: TextView
     lateinit var numberEditText: EditText
@@ -34,40 +27,43 @@ class MainActivity : AppCompatActivity() {
 
         requestQueue = Volley.newRequestQueue(this)
 
-        titleTextView = findViewById<TextView>(R.id.comicTitleTextView)
-        descriptionTextView = findViewById<TextView>(R.id.comicDescriptionTextView)
-        numberEditText = findViewById<EditText>(R.id.comicNumberEditText)
-        showButton = findViewById<Button>(R.id.showComicButton)
-        comicImageView = findViewById<ImageView>(R.id.comicImageView)
+        titleTextView = findViewById(R.id.comicTitleTextView)
+        descriptionTextView = findViewById(R.id.comicDescriptionTextView)
+        numberEditText = findViewById(R.id.comicNumberEditText)
+        showButton = findViewById(R.id.showComicButton)
+        comicImageView = findViewById(R.id.comicImageView)
 
         showButton.setOnClickListener {
             downloadComic(numberEditText.text.toString())
         }
 
+        val cachedComicFile = File(cacheDir, "cached_comic.json")
+        if (cachedComicFile.exists()) {
+            val jsonString = cachedComicFile.readText()
+            val comicObject = JSONObject(jsonString)
+            showComic(comicObject)
+        }
     }
 
-    // Fetches comic from web as JSONObject
-    private fun downloadComic (comicId: String) {
+    private fun downloadComic(comicId: String) {
         val url = "https://xkcd.com/$comicId/info.0.json"
-        requestQueue.add (
-            JsonObjectRequest(url
-                , {showComic(it)}
-                , {}
+        requestQueue.add(
+            JsonObjectRequest(url,
+                { showComic(it) },
+                { }
             )
         )
     }
 
-    // Display a comic for a given comic JSON object
-    private fun showComic (comicObject: JSONObject) {
+    private fun showComic(comicObject: JSONObject) {
         titleTextView.text = comicObject.getString("title")
         descriptionTextView.text = comicObject.getString("alt")
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
+        saveComic(comicObject)
     }
 
-    // Implement this function
     private fun saveComic(comicObject: JSONObject) {
-
+        val file = File(cacheDir, "cached_comic.json")
+        file.writeText(comicObject.toString())
     }
-
-
 }
